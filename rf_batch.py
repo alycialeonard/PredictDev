d99bebef9b41127bd7578e6135cd596a94085dc3
Alycia Leonard <alycia.leonard17@gmail.com>
2025-12-05 17:17:54 +0000

    Batch execution + generic function for different classifier experiments

--- rf_batch.py
+++ rf_batch.py
@@ -6,7 +6,7 @@ Filename: rf.py
 Author: Alycia Leonard
 Date: 2025-12-05
 Version: 1.0
-Description: rf modelling script for UPV dataset - run experiment batch using function from rf.py
+Description: rf modelling script for UPV dataset - run experiment batch using function
 License: GNU GPL-3.0
 Contact: alycia.leonard@eng.ox.ac.uk
 """
@@ -14,27 +14,44 @@ Contact: alycia.leonard@eng.ox.ac.uk
 import pandas as pd
 import os
 import warnings
-from rf import rf_experiment
-from functions import safe_list_parser
+from functions import safe_list_parser, run_experiment
+from sklearn.ensemble import RandomForestClassifier
+from scipy.stats import randint
 
 # Suppress tight layout warnings (inevitable due to SHAP code)
 warnings.filterwarnings("ignore", message=".*Tight layout not applied.*")
 
 # --------- RUN THE EXPERIMENTS ---------- #
 def main():
-    # Get df of experiments to run
+
+    # Get dataframe of experiments to run
     cwd = os.getcwd()
     exp = pd.read_csv(os.path.join(cwd, 'data', 'experiments.csv'), low_memory=False)
+
+    # Define classifier
+    clf = RandomForestClassifier(random_state=42)
+    clf_short = "rf"
+
+    # Define parameters to test for hyperparameters
+    param_dist = {
+        'classifier__n_estimators': randint(200, 801),
+        'classifier__max_depth': [None, 8, 12, 16, 20],
+        'classifier__min_samples_leaf': randint(1, 6),
+        'classifier__min_samples_split': randint(2, 11),
+        'classifier__max_features': ['sqrt', 'log2', 0.2, 0.5],
+        'classifier__class_weight': ['balanced', 'balanced_subsample']
+    }
+
     # Cycle through rows in the dataframe, i.e., experiments to run
     for index, row in exp.iterrows():
-        # Define target for prediction.
+
+        # Define target, short-form of target for filenames, questions to drop
         tar = row['target']
-        # Define short-form of target to use in file saving
         tar_short = row['target_short']
-        # Define questions to drop from predictors - parse safely to prevent list issue
         to_drop = safe_list_parser(row['stems_to_drop'])
+
         # Run the experiment
-        rf_experiment(tar, tar_short, to_drop)
+        run_experiment(tar, tar_short, to_drop, clf, clf_short, param_dist)
 
 
 if __name__ == "__main__":
--- xgb.py
+++ xgb.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 
 """
-Filename: rf.py
+Filename: xgb.py
 Author: Alycia Leonard
 Date: 2025-12-05
 Version: 1.0
-Description: rf modelling script for UPV dataset - run a single experiment
+Description: xgb modelling script for UPV dataset - run a single experiment
 License: GNU GPL-3.0
 Contact: alycia.leonard@eng.ox.ac.uk
 """
 
-from sklearn.ensemble import RandomForestClassifier
 from functions import run_experiment
 from scipy.stats import randint
 import warnings
+from xgboost import XGBClassifier
 
 # Suppress tight layout warnings (inevitable due to SHAP code)
 warnings.filterwarnings("ignore", message=".*Tight layout not applied.*")
@@ -26,30 +26,32 @@ def main():
     tar = 'Which 5 items are most important to you in your daily life? Please indicate these in order of importance, starting with the most important_Electricity'
 
     # Define short-form of target to use in file saving
-    tar_short = 'UPV_Electricity'
+    tar_short = 'upv_electricity'
 
     # Define questions to drop from predictors
     to_drop = ['Which of the following items do you have access to in your daily life?',
                'Which 5 items are most important to you in your daily life? Please indicate these in order of importance, starting with the most important',
                'Given the chosen climate event - which 3 items are most useful to you?']
 
-    # Define classifier
-    clf = RandomForestClassifier(random_state=42)
-    clf_short = "rf"
+    # Define XGBoost classifier (without tuned params). Use_label_encoder=False and eval_metric to avoid deprecation warnings
+    clf = XGBClassifier(use_label_encoder=False, eval_metric='logloss', random_state=42, n_jobs=-1)
 
-    # Define parameters to test for hyperparameters
+    clf_short = 'xgb'
+
+    # Define param_dist to search (for hyperparameters)
     param_dist = {
-        'classifier__n_estimators': randint(200, 801),
-        'classifier__max_depth': [None, 8, 12, 16, 20],
-        'classifier__min_samples_leaf': randint(1, 6),
-        'classifier__min_samples_split': randint(2, 11),
-        'classifier__max_features': ['sqrt', 'log2', 0.2, 0.5],
-        'classifier__class_weight': ['balanced', 'balanced_subsample']
+        'classifier__n_estimators': randint(100, 1001),
+        'classifier__max_depth': randint(3, 16),
+        'classifier__learning_rate': [0.01, 0.03, 0.05, 0.1, 0.2],
+        'classifier__subsample': [0.6, 0.8, 1.0],
+        'classifier__colsample_bytree': [0.6, 0.8, 1.0],
+        'classifier__gamma': [0, 0.1, 0.5, 1],
+        'classifier__reg_alpha': [0, 0.1, 0.5, 1.0],
+        'classifier__reg_lambda': [1, 5, 10]
     }
 
     # Run the experiment
     run_experiment(tar, tar_short, to_drop, clf, clf_short, param_dist)
-
 
 if __name__ == "__main__":
     main()
